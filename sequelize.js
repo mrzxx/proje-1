890c1d3b78d622b9ed55634eb628d697f63ff3b0
Mert PAMUK <76157792+mrzxx@users.noreply.github.com>
2024-01-22 13:15:06 +0300

    Update sequelize.js

--- sequelize.js
+++ sequelize.js
@@ -1,25 +1,7 @@
 const Sequelize = require('sequelize');
-
-// Veritabanı bağlantısı oluşturun
-const sequelize = new Sequelize('proje_iot', 'fecriati', 'mytestpasswordhere2024-0001', {
+const sequelize = new Sequelize('proje_iot', '', '', {
   host: 'localhost',
   dialect: 'mysql', // Veritabanı türünü seçin (örneğin, PostgreSQL, MySQL, vb.)
   timezone: '+03:00' // İstanbul için GMT+3
 });
-
-// sequelize nesnesini dışa aktarın
 module.exports = sequelize;
-//AMAZON
-//admin
-//mytestpasswordhere2023
-
-/*
-CREATE USER 'fecriati'@'localhost' IDENTIFIED BY 'mytestpasswordhere2023';
-GRANT ALL PRIVILEGES ON *.* TO 'fecriati'@'localhost';
-FLUSH PRIVILEGES;
-
-
-CREATE USER 'myuser'@'localhost' IDENTIFIED BY 'mystrongpassword';
-GRANT ALL PRIVILEGES ON mydatabase.* TO 'myuser'@'localhost';
-FLUSH PRIVILEGES;
-*/
